470a642080444516334d1ca4a9809057e7af0acf
yabumoto <1501152st.asojuku.ac.jp>
2017-06-05 14:08:46 +0900

    djgalf

--- app/src/main/java/jp/ac/asojuku/jousisenb/mrwakeup/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/jousisenb/mrwakeup/MainActivity.java
@@ -1,12 +1,19 @@
 package jp.ac.asojuku.jousisenb.mrwakeup;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
     @Override
     protected void onResume(){
