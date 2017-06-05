18f1262bcc31b84cb1210287fa797676e9600558
rrr <1501164@st.asojuku.ac.jp>
2017-06-05 15:20:54 +0900

    牛

--- app/src/main/java/jp/ac/asojuku/jousisenb/mrwakeup/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/jousisenb/mrwakeup/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,15 +20,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume(){
         super.onResume();
 
-        //Button
-        Button button = (Button)findViewById(R.id.strartbutton);
-        //Buttonにリスナーをセット
-        button.setOnClickListener(new View.OnClickListener(){
+        //時計をクリックしたら設定画面に飛ぶように設定
+        TextView textView = (TextView)findViewById(R.id.setTime);
+        //TextViewにリスナーをセット
+        textView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                //ボタンをタップされたら次の画面を表示
+                //時計をタップされたら次の画面を表示
                 Intent intent = new Intent(MainActivity.this,G002.class);
                 startActivity(intent);
+            }
+        });
+
+        //Buttonを認識させる
+        Button buttonAction = (Button)findViewById(R.id.strartbutton);
+        //ボタンにリスナーをセット
+        buttonAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //タップされたらボタンの文字を変更
+                Button button = new Button(startbutton.this,stopbutton.button);
+
             }
         });
     }
